0481d5f51bc2360df3e135bb7914d4f7515d42de
sdr2017 <sarahjeanlinnebur@Sarah-Linneburs-MacBook-Pro.local>
2017-07-11 21:47:06 -0600

    initial commit

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'l08m2dfKZkeVAYcO3Njstddda',
+  consumer_secret: 'aVHG3imi6hDHVxadaF4X8MkcbSg7OZIVGN8EX1TWcI2v0XgJz7',
+  access_token_key: '884916712113668096-ycrynFYhrFToxVNH0NTLCeZRzIoL6h0',
+  access_token_secret: 'lnbxqB4gBmndCukdwCOiIrqOTyZpzzLksLO7c8r203kmo',
+	
+}
---
+++ liri.js
@@ -0,0 +1,20 @@
+var twitterKeys = require("./keys.js");
+console.log(twitterKeys);
+
+var command = (process.argv[2]);
+
+// if (command === 'my-tweets') {
+
+// };
+
+// else if (command === 'spotify-this-song') {
+
+// };
+
+// else if (command === 'movie-this') {
+
+// };
+
+// else if (command === 'do-what-it-says') {
+
+// };
