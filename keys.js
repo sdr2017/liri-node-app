46c31798565ebd4eb3dcbc145273491b36326b44
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-07-15 14:47:47 -0600

    adding files for each app

--- keys.js
+++ keys.js
@@ -1,9 +1,20 @@
 console.log('this is loaded');
 
-exports.twitterKeys = {
+var twitterKeys = {
   consumer_key: 'l08m2dfKZkeVAYcO3Njstddda',
   consumer_secret: 'aVHG3imi6hDHVxadaF4X8MkcbSg7OZIVGN8EX1TWcI2v0XgJz7',
   access_token_key: '884916712113668096-ycrynFYhrFToxVNH0NTLCeZRzIoL6h0',
   access_token_secret: 'lnbxqB4gBmndCukdwCOiIrqOTyZpzzLksLO7c8r203kmo',
 	
+}
+var spotifyKey = {
+
+}
+var omdbKey ={
+
+}
+module.exports = {
+	twitterKeys,
+	spotifyKey,
+	omdbKey
 }
--- liri.js
+++ liri.js
@@ -1,13 +1,8 @@
 var Twitter = require('twitter');
-var twitterKeys = require("./keys.js");
-console.log(twitterKeys);
+var Keys = require("./keys.js");
+console.log(Keys);
 
-var client = new Twitter({
-  consumer_key: 'l08m2dfKZkeVAYcO3Njstddda',
-  consumer_secret: 'aVHG3imi6hDHVxadaF4X8MkcbSg7OZIVGN8EX1TWcI2v0XgJz7',
-  access_token_key: '884916712113668096-ycrynFYhrFToxVNH0NTLCeZRzIoL6h0',
-  access_token_secret: 'lnbxqB4gBmndCukdwCOiIrqOTyZpzzLksLO7c8r203kmo'
-});
+var client = new Twitter(Keys.twitterKeys);
 
 var command = (process.argv[2]);
 
@@ -29,7 +24,10 @@ if (command === 'my-tweets') {
 // };
 
 	function tweets() {
-		client.get("favorites/list", function (err, data, response) {
+		client.get("favorites/list", function (error, tweets, response) {
+			if (error) throw (error);
+			console.log("this is working!");
+		console.log(tweets);
 		console.log(response);
 		});
 	};
