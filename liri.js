3ea9ad2a0b037069e309f3090bf9dd96de88aa2f
sdr2017 <sarahjeanlinnebur@Sarah-Linneburs-MacBook-Pro.local>
2017-07-14 23:28:02 -0600

    trying to make progress with twitter

--- liri.js
+++ liri.js
@@ -1,11 +1,20 @@
+var Twitter = require('twitter');
 var twitterKeys = require("./keys.js");
 console.log(twitterKeys);
 
+var client = new Twitter({
+  consumer_key: 'l08m2dfKZkeVAYcO3Njstddda',
+  consumer_secret: 'aVHG3imi6hDHVxadaF4X8MkcbSg7OZIVGN8EX1TWcI2v0XgJz7',
+  access_token_key: '884916712113668096-ycrynFYhrFToxVNH0NTLCeZRzIoL6h0',
+  access_token_secret: 'lnbxqB4gBmndCukdwCOiIrqOTyZpzzLksLO7c8r203kmo'
+});
+
 var command = (process.argv[2]);
 
-// if (command === 'my-tweets') {
+if (command === 'my-tweets') {
+	tweets();	
 
-// };
+};
 
 // else if (command === 'spotify-this-song') {
 
@@ -18,3 +27,9 @@ var command = (process.argv[2]);
 // else if (command === 'do-what-it-says') {
 
 // };
+
+	function tweets() {
+		client.get("favorites/list", function (err, data, response) {
+		console.log(response);
+		});
+	};
