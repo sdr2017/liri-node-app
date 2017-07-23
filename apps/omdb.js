a1a1874658cbb08c38d5bbc08ea2cac3e034baa3
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-07-22 22:31:47 -0600

    cleaning up movie info display

--- apps/omdb.js
+++ apps/omdb.js
@@ -8,12 +8,31 @@ var myOmdb = function(keys) {
 	function omdb(cb) { 
 		var songOrMovie = (process.argv[3]);
 		var song = songOrMovie || "Mr. Nobody";
-		var queryURL = "http://www.omdbapi.com/?t=" + song + "&y=&plot=short&apikey=" + client;
+		var queryURL = "http://www.omdbapi.com/?t=" + song + "&y=&plot=short&apikey=" + client + "&tomatoes=true";
 
 		request(queryURL, function (error, response, body) {
-		  console.log('body:', body); // Print the HTML 
+
+			var bodyInfo = JSON.parse(body);
+			
+			console.log("Movie title: ", bodyInfo.Title);
+			console.log("IMDB Rating: ", bodyInfo.imdbRating);
+			console.log("Country Produced: ", bodyInfo.Country);
+			console.log("Language: ", bodyInfo.Language);
+			console.log("Plot: ", bodyInfo.Plot);
+			console.log("Actors: ", bodyInfo.Actors);
+			console.log("Rotten Tomatoes URL ", bodyInfo.tomatoURL);
+
 		});
 };
+
+ // * Title of the movie.
+ //   * Year the movie came out.
+ //   * IMDB Rating of the movie.
+ //   * Country where the movie was produced.
+ //   * Language of the movie.
+ //   * Plot of the movie.
+ //   * Actors in the movie.
+ //   * Rotten Tomatoes URL.
 
 	that = {};
 	that.omdb = function(cb){omdb(cb);};
